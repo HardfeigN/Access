2dd3e6e0af977f96f6d8897dc0e2caf2979f95a3
HardfeigN <yarik.yesip@gmail.com>
2023-10-17 14:36:20 +0700

    added repository pattern 3

---
+++ Access_DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using Access_DataAccess.Data;
+using Access_DataAccess.Repository.IRepository;
+using Access_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category obj)
+        {
+            var objFromDb = base.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = obj.Name;
+                objFromDb.ParentId = obj.ParentId;
+            }
+        }
+    }
+}
---
+++ Access_DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using Access_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category obj);
+    }
+}
