b584fa9b512e2447c168ca37f174db87d0fdadb9
HardfeigN <yarik.yesip@gmail.com>
2023-10-22 10:58:08 +0700

    Category management is done (cleared controller)

--- Access/Controllers/CategoryController.cs
+++ Access/Controllers/CategoryController.cs
@@ -81,56 +81,6 @@ namespace Access.Controllers
             return View(categoryVM);
         }
 
-        //GET - CREATE
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        //POST - CREATE
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create(Category obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _catRepos.Add(obj);
-                _catRepos.Save();
-                return RedirectToAction("Index");
-            }
-            return View(obj);
-        }
-
-        //GET - Edit
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            var obj = _catRepos.Find(id.GetValueOrDefault());
-            if (obj == null)
-            {
-                return NotFound();
-            }
-
-            return View(obj);
-        }
-
-        //POST - Edit
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(Category obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _catRepos.Update(obj);
-                _catRepos.Save();
-                return RedirectToAction("Index");
-            }
-            return View(obj);
-        }
-
         //GET - Delete
         public IActionResult Delete(int? id)
         {
